fd12a0dd8abbd6c486a1fed6caaf2916ee88bb49
jon1enforce <tgse2@web.de>
2024-08-15 16:39:27 +0200

    Update reverse.sh
    reverse the local tree....

--- reverse.sh
+++ reverse.sh
@@ -1,2 +1,3 @@
 pipdeptree --freeze --warn silence | grep -P '^[\w0-9-=.]+' > reverse.txt
+#pipdeptree --freeze --warn silence --local-only --reverse | grep -P '^[\w0-9-=.]+' > reverse.txt
 sudo python3 -Xfrozen_modules=off  reverse.py
