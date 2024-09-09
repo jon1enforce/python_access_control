d7a4e885d846851c86c3692c9a1db937bdddb103
jon1enforce <tgse2@web.de>
2024-09-09 23:31:28 +0200

    Update reverse.py
    i cannot resolve the error -- it is about frozen..needs to be fixed..

--- reverse.py
+++ reverse.py
@@ -2,11 +2,18 @@
 #pipdeptree --freeze --warn silence | grep -P '^[\w0-9-=.]+' > reverse.txt
 #command line
 import os
+import sys
+if sys.platform[:5] == 'linux':
+    from landlock import Ruleset
+if sys.platform[:7] == 'openbsd':
+    import openbsd
+
 def reverse():
     import sysconfig
-    #imports_example = ['sys', 'itertools', 'datetime', 'os'] 
-    my_file = open("reverse.txt", "r") 
-    data = my_file.read()  
+    #exmaple imports..
+    #imports = ['sys', 'itertools', 'datetime', 'os']
+    my_file = open("reverse.txt", "r")
+    data = my_file.read()
     imports_source = data.replace('-', '_').split("\n")
     imports = []
     for i in imports_source:
@@ -27,9 +34,40 @@ def reverse():
             print("Error importing ", x, '.')
             continue
         all_modules.append(modules[x])
-    print(all_modules)
-    return all_modules
-#test
+    module_path = []
+    for i in all_modules:
+        i = str(i)
+        x = i.split('from')
+        if len(x)==2:
+            x[1].replace(' ', '')
+            x[1] = x[1][:-1]
+            print(x[1], '\n')
+            module_path.append(x[1])
+    return module_path
+#test on setup.py .. during app install ..
 if __name__=='__main__':
-    reverse()
-#returns /usr/local/lib/python3.11/dist-packages at all..
+    j = '/namespace'
+    list = reverse()
+    print(list)
+    try:
+        rs = Ruleset()
+        for i in list:
+            try:
+                j = str(i.replace('__init__.py', '').replace(' ', '').replace("'", ''))
+                s = '"' + j + '"'
+                print(s)
+                rs.allow(s)
+            except:
+                print('error..')
+        rs.apply()
+        print('..reverse..on..setup.py..set..linux-landlock')
+        if sys.platform == 'openbsd':
+            openbsd.unveil('/*', 'r')
+            openbsd.unveil('.', 'rwx')
+            for i in list:
+                openbsd.unveil(os.path.abspath(i))
+            print('..reverse..on..setup.py..set..openbsd..unveil')
+    except:
+        print('no suitable OS, try linux or openbsd')
+        sys.exit()
+#use the absolute path for access control
