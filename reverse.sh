1b8a979fb1c40932f9e33ede2ce3b8912ed048ca
jon1enforce <tgse2@web.de>
2024-09-10 23:36:07 +0200

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -1,3 +1,4 @@
 pipdeptree --freeze --warn silence | grep -P '^[\w0-9-=.]+' > reverse.txt
 #pipdeptree --freeze --warn silence --local-only --reverse | grep -P '^[\w0-9-=.]+' > reverse.txt
 sudo python3 -Xfrozen_modules=off  reverse.py
+sudo python3 reverse_ruleset.py
