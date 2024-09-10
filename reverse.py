4c98bbcafe401377346fe7a7fc0503b597bcf35b
jon1enforce <tgse2@web.de>
2024-09-10 23:36:50 +0200

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -3,10 +3,6 @@
 #command line
 import os
 import sys
-if sys.platform[:5] == 'linux':
-    from landlock import Ruleset
-if sys.platform[:7] == 'openbsd':
-    import openbsd
 
 def reverse():
     import sysconfig
@@ -41,33 +37,14 @@ def reverse():
         if len(x)==2:
             x[1].replace(' ', '')
             x[1] = x[1][:-1]
-            print(x[1], '\n')
+            #print(x[1], '\n')
             module_path.append(x[1])
     return module_path
 #test on setup.py .. during app install ..
 if __name__=='__main__':
-    j = '/namespace'
     list = reverse()
-    print(list)
-    try:
-        rs = Ruleset()
-        for i in list:
-            try:
-                j = str(i.replace('__init__.py', '').replace(' ', '').replace("'", ''))
-                s = '"' + j + '"'
-                print(s)
-                rs.allow(s)
-            except:
-                print('error..')
-        rs.apply()
-        print('..reverse..on..setup.py..set..linux-landlock')
-        if sys.platform == 'openbsd':
-            openbsd.unveil('/*', 'r')
-            openbsd.unveil('.', 'rwx')
-            for i in list:
-                openbsd.unveil(os.path.abspath(i))
-            print('..reverse..on..setup.py..set..openbsd..unveil')
-    except:
-        print('no suitable OS, try linux or openbsd')
-        sys.exit()
-#use the absolute path for access control
+    file = open('./setting.txt','w')
+    for i in list:
+        file.write(i)
+        file.write('\n')
+    file.close()
